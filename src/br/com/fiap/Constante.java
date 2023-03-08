9e01f542e5c8a3014e13df6ed930bc9c157414bc
ticoulrych <ticoulrych@gmail.com>
2023-03-08 14:53:57 -0300

    first commit

---
+++ src/br/com/fiap/Constante.java
@@ -0,0 +1,15 @@
+package br.com.fiap;
+
+public class Constante {
+
+	public static void main(String[] args) {
+		final double PI = 3.1416; 
+		final float GRAVIDADE_TERRESTRE = 9.8f;
+		
+		System.out.println("Valor de PI é: " + PI);
+		System.out.println("A gravidade terrestre é: " + GRAVIDADE_TERRESTRE);
+		
+		
+	}
+
+}
---
+++ src/br/com/fiap/Primitivo.java
@@ -0,0 +1,30 @@
+package br.com.fiap;
+
+public class Primitivo {
+
+	public static void main(String[] args) {
+		//decração de variaveis  (atribuição de valores)
+	char sexo = 'F'; 
+	byte idade = 25;
+    short codigo = 15650;
+    float media = 8.7f;
+    int alunos = 50, classes = 11;
+    long brasileiros = 216535748; 
+    double dolar = 5.15;
+    boolean alternativa = false;
+    String frase = "hello world";
+    
+    System.out.println("Sexo: " + sexo + "\nIdade: " + idade +"\nCodigo: " + codigo + "\nMedia: " + media 
+    		+ "\nAlunos: " + alunos + "\nClasses: " + classes);
+    System.out.println("Habitantes: " + brasileiros + "\nCotaçao do dolar: " + dolar);
+    System.out.println("Alternativa " + alternativa);
+    System.out.println("Frase do dia: " + frase);
+    
+  
+  
+    		
+    
+    
+	}
+
+}
